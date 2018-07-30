bfaeefd3dd96df71e164d92bb0ddfb3e8b259bb4
Rajat Choudhary <choudharyrajat9395@gmail.com>
2018-07-30 22:10:55 +0530

    updated

---
+++ C Coding/hello.c
@@ -0,0 +1,7 @@
+// C program to print hello world without using semicolon
+
+#include<stdio.h>
+void main(){
+    if(printf("Hello world")){
+    }
+}
---
+++ C Coding/hello2.c
@@ -0,0 +1,6 @@
+//C program to print hello world without using semicolon
+#include<stdio.h>
+void main(){
+    while(!printf("Hello world")){
+    }
+}
