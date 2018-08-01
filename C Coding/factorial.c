66277a8b06372a8a81e19f62e84fad917e579fdd
Rajat Choudhary <choudharyrajat9395@gmail.com>
2018-08-01 21:14:40 +0530

    updated

---
+++ C Coding/factorial.c
@@ -0,0 +1,14 @@
+// Program to find  factorial of a number
+#include<stdio.h>
+int main(){
+  int i,f=1,num;
+ 
+  printf("Enter a number: ");
+  scanf("%d",&num);
+ 
+  for(i=1;i<=num;i++)
+      f=f*i;
+ 
+  printf("Factorial of %d is: %d",num,f);
+  return 0;
+}
